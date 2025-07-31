d049837cc81654182adf5da3882e0ef22fbd2df4
Ell3nSantos <ellengloria15@gmail.com>
2025-07-30 21:54:17 -0300

    Add files via upload

---
+++ aula5.py
@@ -0,0 +1,26 @@
+numeros1 = list(range (1,11))
+print (numeros1)
+
+
+pares = list(range(2,22,2))
+print (pares)
+
+
+lista_numeros = list(range(1,11))
+print (lista_numeros)
+
+
+terceiro_elemento = lista_numeros
+print (terceiro_elemento[2])
+
+
+lista_numeros.append(9)
+print (lista_numeros)
+
+
+lista_numeros.remove(5)
+print (lista_numeros)
+
+
+lista_carros = ['fox, astra, hb20']
+print (lista_carros)
