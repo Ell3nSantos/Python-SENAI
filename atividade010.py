9479ffc1246899d549e1c569f6ff0cd47c8bd7d1
Ell3nSantos <ellengloria15@gmail.com>
2025-08-06 21:54:32 -0300

    Add files via upload

---
+++ atividade010.py
@@ -0,0 +1,35 @@
+# ATIVIDADE1
+# contador  = 0
+# while contador <= 1000:
+#     print ('numero', contador)
+#     contador = contador +1
+
+
+# contador = 0
+# list = []
+# while contador <= 10:
+#     print ('nome:', list)
+#     nomes = input ('nome:')
+#     list.append (nomes)
+#     contador = contador +1
+
+
+
+# ATIVIDADE2
+dados = {}
+print ('login')
+nome  = input('nome:')
+notas = int(input ('nota:'))
+senha = input ('senha:')
+
+dados['nome:'] = [nome]
+dados['nota:'] = [notas]
+dados['senha:'] = [senha]
+
+print ('Dados inseridos:', dados)
+
+for n in range (3): 
+    nome  = input('nome:')
+    notas = int(input ('nota:'))
+    senha = input ('senha:')
+
